66d13e69cf9d03ad0ce97c5f898c35b6c932ca31
slim01 <s.slimani@gmx.de>
2019-09-19 18:27:30 +0200

    final website

--- js/chart.js
+++ js/chart.js
@@ -1,32 +1,91 @@
-var ctx = $('#tempChart');
-var tempChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [23,24,25,26,27,28,29],
-      datasets: [{ 
-          data: [10,12,13,14,10,11,13],
-          label: "Münster",
-          borderColor: "#3e95cd",
-          fill: false
-        }, { 
-          data: [10,11,13,8,9,10, 11],
-          label: "Havixbeck",
-          borderColor: "#8e5ea2",
-          fill: false
-        }
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Temperatur in Münster und Havixbeck vom 23.10.19 - 29.10.19'
-      },
-      scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
+var senseboxIds = ["59ba6c8ad67eb50011922fff", "5a0c2cc89fd3c200111118f0"];
+var tempSensorIds = ["59ba6c8ad67eb50011923006", "5a0c2cc89fd3c200111118f7"];
+/*get sensebox data*/
+var x = 0;
+var requestsArray = [];
+var fromDate = new Date();
+fromDate.setMonth(fromDate.getMonth() - 1);
+fromDate = fromDate.toISOString();
+console.log(fromDate);
+for(x; x < senseboxIds.length; x++){
+  requestsArray.push($.get( "https://api.opensensemap.org/boxes/" + senseboxIds[x] + "/data/" + tempSensorIds[x] + "?from-date=" + fromDate ));
+}
+$.when.apply($, requestsArray).done(function (data1, data2) {
+  console.log(data1);
+  console.log(data2);
+  chart(data1[0], data2[0]);
 });
+function chart(data1, data2){
+var tempDataH = [];
+var tempDataM = [];
+var x = 0;
+var y = 0;
+for (var i = 0; i < data1.length; i++) {
+    y = y + parseFloat(data1[i].value);
+    if(i % 199 == 0 && i != 0){ 
+        console.log(y);
+        var value = {
+        x: data1[i -50].createdAt ,
+        y: y/200,
+        }
+        tempDataH.push(value);
+        y = 0;
+    }
+}
+for (var j = 0; j < data2.length; j++) {
+    x = x + parseFloat(data2[j].value);
+    if(j % 99 == 0 && j != 0){
+        console.log(x);  
+        var value = {
+        x: data2[j -50].createdAt,
+        y: x/100,
+    }
+        tempDataM.push(value);
+        x = 0;
+    }
+}
+
+var timeFormat = 'YYYY-MM-DDTHH:mm:ss.sssZ';
+  var ctx = $('#tempChart');
+  var tempChart = new Chart(ctx, {
+        type:'line',
+        data:{
+            datasets: [
+                {
+                    label: "Nottuln-Darup",
+                    data: tempDataH,
+                    fill: false,
+                    borderColor: "#3e95cd"
+                },
+                {
+                    label: "Münster",
+                    data: tempDataM,
+                    fill: false,
+                    borderColor: "#8e5ea2"
+
+                }
+            ]
+        },
+        options: {
+            
+            scales:     {
+                xAxes: [{
+                    type:"time",
+                    time:{
+                        parser: timeFormat,
+                        tooltipFormat: 'll'
+                    },
+                    scaleLabel: {
+                        display:true,
+                        labelString: 'Datum'
+                    }
+                }],
+                yAxes: [{
+                    scaleLabel: {
+                        display:     true,
+                        labelString: 'Temperatur in °C'
+                    }
+                }]
+            }
+        }
+});}
---
+++ js/map_fullscreen.js
@@ -0,0 +1,125 @@
+"use strict";
+loadData();
+function createPolylinePointsTemp (data) {
+	return data.map(function (item) {
+			var trkpt = L.latLng(item.Lat, item.Long, item.Temperature);
+			//trkpt.meta = item.meta;
+			return trkpt;
+	});
+}
+function createPolylinePointsHumidity (data) {
+	return data.map(function (item) {
+		var trkpt = L.latLng(item.Lat, item.Long, item.Humidity);
+		// trkpt.meta = item.meta;
+		return trkpt;
+	});
+}
+function createPolylinePointsPM2 (data) {
+	return data.map(function (item) {
+			var trkpt = L.latLng(item.Lat, item.Long, item.PM25);
+			return trkpt;
+	});
+}
+
+function loadData () {
+	$.ajax({
+    url: 'https://raw.githubusercontent.com/Ifgi-AFG/Ifgi-AFG.github.io/master/Data_collected_Sensebox.csv',
+    dataType: 'text',
+    }).done(successFunction);        
+}
+function successFunction(data){
+	var data1 = $.csv.toObjects(data);
+	console.log(data1);
+	for (var i = 0; i < 100; i++) {
+	data1.forEach((line, i) => {		
+		if(line.Lat === "0" || line.Long === "0" ){
+			data1.splice(i, 1);
+		}
+	})};
+	console.log(data1);
+	var tempPolylines = createPolylinePointsTemp(data1);
+	var pmPolylines = createPolylinePointsPM2(data1);
+	console.log(pmPolylines);
+	createPolylineOptionsPoints(tempPolylines,pmPolylines);
+}
+
+
+/* init map */
+var map = L.map('mapid').setView([51.965, 7.600], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+/*get sensebox data*/
+function createPolylineOptionsPoints (polylines1,polylines2){
+
+	var tempPolylines = 
+L.multiOptionsPolyline(polylines1, {
+    multiOptions: {
+        optionIdxFn: function (latLng) {
+            var i,
+                altThresholds = [9, 12, 15, 18, 21, 24, 27 , 30];
+
+            for (i = 0; i < altThresholds.length; ++i) {
+                if (latLng.alt <= altThresholds[i]) {
+                    return i;
+                }
+            }
+            return altThresholds.length;
+        },
+        options: [
+            {color: '#0000FF'}, {color: '#0040FF'}, {color: '#0080FF'},
+            {color: '#00FFB0'}, {color: '#00E000'}, {color: '#80FF00'},
+            {color: '#FFFF00'}, {color: '#FFC000'}, {color: '#FF0000'}
+        ]
+    },
+    weight: 5,
+    lineCap: 'butt',
+    opacity: 0.75,
+    smoothFactor: 1});
+	var pmPolylines = L.multiOptionsPolyline(polylines2, {
+    multiOptions: {
+        optionIdxFn: function (latLng) {
+            var i,
+                altThresholds = [2.5, 2.7, 2.9, 3.1, 3.3, 3.5, 3.7 , 3.9];
+
+            for (i = 0; i < altThresholds.length; ++i) {
+                if (latLng.alt <= altThresholds[i]) {
+                    return i;
+                }
+            }
+            return altThresholds.length;
+        },
+        options: [
+            {color: '#0000FF'}, {color: '#0040FF'}, {color: '#0080FF'},
+            {color: '#00FFB0'}, {color: '#00E000'}, {color: '#80FF00'},
+            {color: '#FFFF00'}, {color: '#FFC000'}, {color: '#FF0000'}
+        ]
+    },
+    weight: 5,
+    lineCap: 'butt',
+    opacity: 0.75,
+    smoothFactor: 1});
+    var  layers = {
+		"Temperatur": tempPolylines,
+		"Feinstaub": pmPolylines
+	}
+    tempPolylines.addTo(map);
+    L.control.layers(layers,null,{collapsed:false}).addTo(map);
+
+}
+var legend = L.control({position: 'bottomleft'});
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend');
+    /*var labels = ['<strong>Categories</strong>'],
+    categories = ['Low','Signage','Line Markings','Roadside Hazards','Other'];
+
+    for (var i = 0; i < categories.length; i++) {
+        div.innerHTML += 
+        labels.push('<i class="circle" style="background:' + getColor(categories[i]) + '"></i> ' +  (categories[i] ? categories[i] : '+'));
+    }
+    div.innerHTML = labels.join('<br>');*/
+    div.innerHTML = "<div style='display:inline; background-color: white;'><img src='images/Unterwegs_ChatBot.jpg' style=' width: 20%'><br> Die angezeigten Daten wurden mit Hilfe von E-Scootern und Senseboxen gesammelt.<br>Niedrig <img style='width: 10%; height: 5%;'src='images/legendColor.png'> Hoch</div>";
+    return div;
+};
+legend.addTo(map);
